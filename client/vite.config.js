5fd8b82b3224037c2bd2bb640ee84d4894cc5dc6
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-11-25 15:45:17 -0600

    updating the client folder to get railway working

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: process.env.PORT || 3000
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
