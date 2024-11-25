5c062fb8564e03a27deb2ae234199e209548bdae
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-11-25 12:45:28 -0600

    updated to bcryptjs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const pool = require('../db');
 const auth = require('../middleware/auth');
