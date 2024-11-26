6ff37a7a0cbdc33523629e456919db544b2ebf0d
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-11-26 15:20:45 -0600

    ooooo

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,6 @@ console.log('Config module loading...');
 
 const API_URL = import.meta.env.PROD 
   ? 'https://esd-testing-app-production.up.railway.app'
-  : 'http://localhost:5001';
+  : 'http://localhost:3000';
 
 export default API_URL; 
