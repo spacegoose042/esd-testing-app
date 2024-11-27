b48acc1c11f058fbc22f135919a3dee716bdcc28
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-11-27 08:38:41 -0600

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import config from '../config';
 import { login as authLogin } from '../services/auth';
+import config from '../config';
 
 function Login() {
     const [email, setEmail] = useState('');
