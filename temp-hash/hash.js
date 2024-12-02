d3d332a046cc73252ee3ea9a24c34753a9c9e375
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-12-02 09:17:14 -0600

    cleaning

--- temp-hash/hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'Admin123!';
-
-bcrypt.genSalt(10).then(salt => {
-    bcrypt.hash(password, salt).then(hash => {
-        console.log('Use this hash:', hash);
-    });
-});
