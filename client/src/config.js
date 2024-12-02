74fe30b19042c270a974433fef5ffd9cb26a0ecb
spacegoose042 <94691697+spacegoose042@users.noreply.github.com>
2024-12-02 12:16:30 -0600

    summary

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,4 @@ const config = {
   apiUrl: import.meta.env.VITE_API_URL
 };
 
-console.log('Current environment:', import.meta.env.MODE);
-console.log('API URL:', config.apiUrl);
-
 export default config; 
